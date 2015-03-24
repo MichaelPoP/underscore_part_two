d30715dea1ea5f72a20adacb1109274a084c8120
Elie Schoppik <eschoppik@gmail.com>
2015-03-24 02:00:09 -0700

    adding original notes

--- src/underscore.js
+++ src/underscore.js
@@ -1,44 +1,41 @@
 var myFunctions = {
 
+  // Extend a given object with all the properties of the passed in
+  // object(s).
+  //
+  // Example:
+  //   var obj1 = {key1: "something"};
+  //   _.extend(obj1, {
+  //     key2: "something new",
+  //     key3: "something else new"
+  //   }, {
+  //     bla: "even more stuff"
+  //   }); // obj1 now contains key1, key2, key3 and bla
+  extend: function(obj) {
 
-   extend: function(obj) {
-    for(var i = 1; i < arguments.length; i++) {
-      for(var prop in arguments[i]) {
-        if(arguments[i].hasOwnProperty(prop)) {
-          obj[prop] = arguments[i][prop];
-        }
-      }
-    }
-
-    return obj;
   },
 
   // Return a function that can be called at most one time. Subsequent calls
   // should return the previously returned value.
-  once: function(func) {
-    // TIP: These variables are stored in a "closure scope" (worth researching),
-    // so that they'll remain available to the newly-generated function every
-    // time it's called.
-    var alreadyCalled = false;
-    var result;
 
-    // TIP: We'll return a new function that delegates to the old one, but only
-    // if it hasn't been called before.
-    return function() {
-      if (!alreadyCalled) {
-        // TIP: .apply(this, arguments) is the standard way to pass on all of the
-        // information from one function call to another.
-        result = func.apply(this, arguments);
-        alreadyCalled = true;
-      }
-      // The new function always returns the originally computed result.
-      return result;
-    };
+  once: function(func) {
+
+  var alreadyCalled = false;
+  var result;
+
+  // TIP: We'll return a new function that delegates to the old one, but only
+  // if it hasn't been called before.
+  return function() {
+
+    // TIP: .apply(this, arguments) is the standard way to pass on all of the
+    // information from one function call to another.
+
+    // The new function always returns the originally computed result.
+
+   };
+
   },
 
-
-  // Like first, but for the last elements. If n is undefined, return just the
-  // last element.
   // Delays a function for the given number of milliseconds, and then calls
   // it with the arguments supplied.
   //
@@ -46,30 +43,19 @@ var myFunctions = {
   // parameter. For example _.delay(someFunction, 500, 'a', 'b') will
   // call someFunction('a', 'b') after 500ms
   delay: function(func, wait) {
-  //create an array of arguments starting with index 2 (skip the function and timeout)
-    var args = Array.prototype.slice.apply(arguments,[2]);
-    //set a timeout and call the function and use the window object for "this" (doesn't matter if null or undefined...well sort of because we're not in strict mode)
-    setTimeout(function(){
-      func.apply(this,args);
-    },wait);
+
   },
 
+  // Memoize an expensive function by storing its results. You may assume
+  // that the function takes only one argument and that it is a primitive.
+  //
+  // _.memoize should return a function that when called, will check if it has
+  // already computed the result for the given argument and return that value
+  // instead if possible.
+  // http://addyosmani.com/blog/faster-javascript-memoization/
   memoize: function(func) {
-    var cache = {};
-    return function() {
-      var args = Array.prototype.slice.call(arguments);
-
-      if(args in cache) {
-        return cache[args];
-      }
-      else {
-        return (cache[args] = func.apply(this,args));
-      }
-
-    };
 
   }
-
 
 };
 module.exports = myFunctions;

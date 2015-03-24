5a5807fef3a202c38934ef2bbe49a82d823441d7
Elie Schoppik <eschoppik@gmail.com>
2015-03-24 01:45:35 -0700

    adding part 2

--- spec/test.js
+++ spec/test.js
@@ -1,79 +1,144 @@
 var myFunctions = require("../src/underscore");
 var expect = require("chai").expect;
-_ = require("underscore");
+var sinon = require("sinon");
 
-describe('first', function() {
-  it('should be able to pull out the first element of an array', function() {
-    expect(myFunctions.first([1,2,3])).to.equal(1);
+describe('extend', function() {
+  it('returns the first argument', function() {
+    var to = {};
+    var from = {};
+    var extended = myFunctions.extend(to, from);
+
+    expect(extended).to.equal(to);
   });
 
-  it('should be able to accept a user-defined index first([1,2],0) should return 1', function() {
-    expect(myFunctions.first([1,2,3], 0)).to.eql([]);
-    expect(myFunctions.first([1,2,3], 2)).to.eql([1, 2]);
-    expect(myFunctions.first([1,2,3], 5)).to.eql([1, 2, 3]);
-  });
-});
+  it('should extend an object with the attributes of another', function() {
+    var to = {};
+    var from = {a:'b'};
+    var extended = myFunctions.extend(to, from);
 
-describe('last', function() {
-  it('should pull the last element from an array', function() {
-    expect(myFunctions.last([1,2,3])).to.equal(3);
+    expect(extended.a).to.equal('b');
   });
 
-  it('should accept an index argument', function() {
-    expect(myFunctions.last([1,2,3], 2)).to.eql([2, 3]);
+  it('should override properties found on the destination', function() {
+    var to = {a:'x'};
+    var from = {a:'b'};
+    var extended = myFunctions.extend(to, from);
+
+    expect(extended.a).to.equal('b');
   });
 
-  it('should return nothing if zero is passed in as the index', function() {
-    expect(myFunctions.last([1,2,3], 0)).to.eql([]);
+  it('should not override properties not found in the source', function() {
+    var to = {x:'x'};
+    var from = {a:'b'};
+    var extended = myFunctions.extend(to, from);
+
+    expect(extended.x).to.equal('x');
   });
 
-  it('should return all the array\'s elements if the index argument is larger than the length of the array', function() {
-    expect(myFunctions.last([1,2,3], 5)).to.eql([1, 2, 3]);
-  });
-});
+  it('should extend from multiple source objects', function() {
+    var extended = myFunctions.extend({x:1}, {a:2}, {b:3});
 
-describe('uniq', function() {
-  it('should return all unique values contained in an unsorted array', function() {
-    var list = [1, 2, 1, 3, 1, 4];
-
-    expect(myFunctions.uniq(list)).to.eql([1, 2, 3, 4]);
+    expect(extended).to.eql({x:1, a:2, b:3});
   });
 
-  it('should handle iterators that work with a sorted array', function() {
-    var iterator = function(value) { return value +1; };
-    var list = [1, 2, 2, 3, 4, 4];
+  it('in the case of a conflict, it should use the last property\'s values when extending from multiple source objects', function() {
+    var extended = myFunctions.extend({x:'x'}, {a:'a', x:2}, {a:1});
 
-    expect(myFunctions.uniq(list, true, iterator)).to.eql([1, 2, 3, 4]);
+    expect(extended).to.eql({x:2, a:1});
   });
-});
 
+  it('should copy undefined values', function() {
+    var extended = myFunctions.extend({}, {a: undefined, b: null});
 
-describe('pluck', function() {
-  it('should return values contained at a user-defined property', function() {
-    var people = [
-      {name : 'moe', age : 30},
-      {name : 'curly', age : 50}
-    ];
-
-    expect(myFunctions.pluck(people, 'name')).to.eql(['moe', 'curly']);
+    expect('a' in extended && 'b' in extended).to.equal(true);
   });
 });
 
-describe('contains', function() {
-  it('should return false if a collection does not contain a user-specified value', function() {
-    expect(myFunctions.contains([4,5,6], 2)).to.equal(false);
-  });
+describe('once', function() {
+  it('should only run a user-defined function if it hasn\'t been run before', function() {
+    var num = 0;
+    var increment = myFunctions.once(function() {
+      num += 1;
+    });
 
-  it('should return true if a collection contains a user-specified value', function() {
-    expect(myFunctions.contains([  4,   5,   6], 5)).to.equal(true);
-  });
+    increment();
+    increment();
 
+    expect(num).to.equal(1);
+  });
 });
 
-describe('flatten', function() {
-  it('can flatten nested arrays', function() {
-    var nestedArray = [[[[[[[[[[[[[[[[[[[[[[[[[[[[1, [2], [[[[3]]], [[[4]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]];
+describe('delay', function() {
+  var clock;
 
-    expect(myFunctions.flatten(nestedArray)).to.eql([1,2,3,4]);
+  beforeEach(function() {
+    clock = sinon.useFakeTimers();
+  });
+
+  afterEach(function() {
+    clock.restore();
+  });
+
+  it('should only execute the function after the specified wait time', function() {
+    var callback = sinon.spy();
+    myFunctions.delay(callback, 100);
+
+    clock.tick(99);
+
+    expect(callback.notCalled).to.equal(true);
+
+    clock.tick(1);
+
+    expect(callback.calledOnce).to.equal(true);
+  });
+
+  it('should have successfully passed function arguments in', function() {
+    var callback = sinon.spy();
+
+    myFunctions.delay(callback, 100, 1, 2);
+    clock.tick(100);
+
+    expect(callback.calledWith(1, 2)).to.equal(true);
+  });
+});
+
+describe('memoize', function() {
+  var fib, fastFib, timeCheck, fastTime, wait;
+
+  beforeEach(function() {
+    fib = function(n) {
+      if(n < 2){ return n; }
+      return fib(n - 1) + fib(n - 2);
+    };
+    fastFib = myFunctions.memoize(fib);
+
+    timeCheck = function(str) { return str + Date.now(); };
+    fastTime = myFunctions.memoize(timeCheck);
+
+    // Synchronous sleep function
+    wait = function(t) {
+      var start = Date.now();
+      while ((Date.now() - start) < t){}
+    };
+  });
+
+  it('a memoized function should produce the same result when called with the same arguments', function() {
+    expect(fib(10)).to.equal(55);
+    expect(fastFib(10)).to.equal(55);
+  });
+
+  it('should give different results for different arguments', function() {
+    expect(fib(10)).to.equal(55);
+    expect(fastFib(10)).to.equal(55);
+    expect(fastFib(7)).to.equal(13);
+  });
+
+  it('should not run the function twice for the same given argument', function() {
+    var firstTime = timeCheck('shazaam!');
+    wait(5);
+    var secondTime = fastTime('shazaam!');
+    wait(5);
+    expect(firstTime).to.not.equal(secondTime);
+    expect(fastTime('shazaam!')).to.equal(secondTime);
   });
 });
--- src/underscore.js
+++ src/underscore.js
@@ -1,42 +1,75 @@
 var myFunctions = {
 
-  // Return an array of the first n elements of an array. If n is undefined,
-  // return just the first element.
-  first: function(array, n) {
 
+   extend: function(obj) {
+    for(var i = 1; i < arguments.length; i++) {
+      for(var prop in arguments[i]) {
+        if(arguments[i].hasOwnProperty(prop)) {
+          obj[prop] = arguments[i][prop];
+        }
+      }
+    }
+
+    return obj;
   },
+
+  // Return a function that can be called at most one time. Subsequent calls
+  // should return the previously returned value.
+  once: function(func) {
+    // TIP: These variables are stored in a "closure scope" (worth researching),
+    // so that they'll remain available to the newly-generated function every
+    // time it's called.
+    var alreadyCalled = false;
+    var result;
+
+    // TIP: We'll return a new function that delegates to the old one, but only
+    // if it hasn't been called before.
+    return function() {
+      if (!alreadyCalled) {
+        // TIP: .apply(this, arguments) is the standard way to pass on all of the
+        // information from one function call to another.
+        result = func.apply(this, arguments);
+        alreadyCalled = true;
+      }
+      // The new function always returns the originally computed result.
+      return result;
+    };
+  },
+
 
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
-  last: function(array, n) {
-
+  // Delays a function for the given number of milliseconds, and then calls
+  // it with the arguments supplied.
+  //
+  // The arguments for the original function are passed after the wait
+  // parameter. For example _.delay(someFunction, 500, 'a', 'b') will
+  // call someFunction('a', 'b') after 500ms
+  delay: function(func, wait) {
+  //create an array of arguments starting with index 2 (skip the function and timeout)
+    var args = Array.prototype.slice.apply(arguments,[2]);
+    //set a timeout and call the function and use the window object for "this" (doesn't matter if null or undefined...well sort of because we're not in strict mode)
+    setTimeout(function(){
+      func.apply(this,args);
+    },wait);
   },
 
-  // Produce a duplicate-free version of the array.
-  uniq: function(array) {
+  memoize: function(func) {
+    var cache = {};
+    return function() {
+      var args = Array.prototype.slice.call(arguments);
 
-  },
+      if(args in cache) {
+        return cache[args];
+      }
+      else {
+        return (cache[args] = func.apply(this,args));
+      }
 
-  // Takes an array of objects and returns and array of the values of
-  // a certain property in it. E.g. take an array of people objects
-  // (which have a name and an age) and return an array of just their ages
-  pluck: function(array, key) {
-
-  },
-
- // Determine if the array contains a given value (using `===`).
- // TIP: There are multiple solutions to this problem, ranging from one line to using reduce();
-  contains: function(array, target) {
-
-  },
-
-  // Turns a multidimensional array into a single array
-  // flatten([1,2,[3,4,[5,6]]]) returns [1,2,3,4,5,6]
-  // Hint: Use Array.isArray to check if something is an array
-  // Also, you will need to use recursion
-  // when you find an array inside of an array
-  flatten: function(array) {
+    };
 
   }
+
+
 };
 module.exports = myFunctions;
